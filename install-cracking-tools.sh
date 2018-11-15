8ac75f02e52aa54d6a41730f298eb7d09491bc9e
Plamen Valov <plamen.valov@isobar.com>
2018-11-15 09:51:31 +0200

    Fix opencl driver install

--- install-cracking-tools.sh
+++ install-cracking-tools.sh
@@ -26,11 +26,6 @@ OLDDIR=$(pwd)
 
 sudo apt-get -y install build-essential
 
-# TO-DO: Test if it depends on opencl_runtime
-false && sudo apt-get -y install ocl-icd-libopencl1 \
-     ocl-icd-dev \
-     ocl-icd-opencl-dev
-
 sudo apt-get -y install \
     medusa \
     ncrack \
@@ -52,25 +47,11 @@ if [[ ! -e $REPODIR/pack ]]; then
     git clone https://github.com/iphelix/pack.git
 fi
 
-# TO-DO: These drivers does not work
-if false
-then
-    echo "[*] Install intel-*.deb drivers from official GitHub repo"
-    [ ! -e "./intel-opencl-drivers" ] && mkdir ./intel-opencl-drivers
-    cd intel-opencl-drivers
+# Needed by hashcat
+sudo apt-get -y ocl-icd-opencl-dev
 
-    wget https://github.com/intel/compute-runtime/releases/download/18.44.11760/intel-gmmlib_18.4.0.326_amd64.deb
-    wget https://github.com/intel/compute-runtime/releases/download/18.44.11760/intel-igc-core_18.43.999_amd64.deb
-    wget https://github.com/intel/compute-runtime/releases/download/18.44.11760/intel-igc-opencl_18.43.999_amd64.deb
-    wget https://github.com/intel/compute-runtime/releases/download/18.44.11760/intel-opencl_18.44.11760_amd64.deb
-
-    sudo dpkg -i intel-*.deb
-
-    cd -
-fi
-
-# TO-DO: Needs to be tested..
-if false
+# Install driver for opencl (needed for hashcat)
+if [ ! -e /opt/intel/opencl-1.2-6.4.0.37 ]
 then
     OLDDIR_1=$(pwd)
 
@@ -81,8 +62,8 @@ then
 
     wget http://registrationcenter-download.intel.com/akdlm/irc_nas/12556/opencl_runtime_16.1.2_x64_rh_6.4.0.37.tgz
     tar zxvf opencl_runtime_*.tgz
-    cd opencl_runtime_*
-    sudo ./install.sh
+    cd opencl_runtime_16.1.2_x64_rh_6.4.0.37
+    sudo ./install.sh -s "$SCRIPT_DIR/conf/opencl_runtime_silent.cfg"
 
     cd $OLDDIR_1
 fi
