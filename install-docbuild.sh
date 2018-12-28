fbabdc1733561ab2ee5c024f16d92fe47fa4d77c
mr6r4y <mr6r4y@gmail.com>
2018-12-29 00:53:41 +0200

    Add

---
+++ install-docbuild.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#: Title: install-docbuild.sh
+#: Description: Installation of pandoc and latex needed to build documents out of Markdown
+#: References:
+
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+
+source "$SCRIPT_DIR/lib/common.sh"
+source "$SCRIPT_DIR/config.sh"
+
+set -e
+
+OLDDIR=$(pwd)
+
+sudo apt-get -y install \
+    texlive-base \
+    texlive-binaries \
+    texlive-extra-utils \
+    texlive-font-utils \
+    texlive-fonts-extra \
+    texlive-fonts-extra-links \
+    texlive-fonts-recommended \
+    texlive-generic-recommended \
+    texlive-lang-cyrillic \
+    texlive-latex-base \
+    texlive-latex-extra \
+    texlive-latex-recommended \
+    texlive-pictures \
+    texlive-plain-generic \
+    texlive-pstricks \
+    texlive-publishers \
+    texlive-xetex \
+    texlive-fonts-extra-doc \
+    texlive-fonts-recommended-doc \
+    texlive-latex-base-doc \
+    texlive-latex-extra-doc \
+    texlive-latex-recommended-doc \
+    texlive-pictures-doc \
+    texlive-pstricks-doc \
+    texlive-publishers-doc \
+    pandoc \
+    xzdec
+
