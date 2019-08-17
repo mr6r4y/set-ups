0aa96a410425b6f8e701bde36ca629c0889f678b
mr6r4y <mr6r4y@gmail.com>
2019-08-17 19:35:19 +0300

    Add docker-composer

--- install-docker.sh
+++ install-docker.sh
@@ -36,7 +36,7 @@ sudo add-apt-repository \
 
 echo "[*] Install Docker CE"
 sudo apt update
-sudo apt install -y docker-ce docker-ce-cli containerd.io
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose
 
 echo "[*] Manage groups"
 sudo groupadd docker
