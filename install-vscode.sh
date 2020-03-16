65670dfea2732df454ed5cfcd9ed9a3db7fccaff
mr6r4y <mr6r4y@gmail.com>
2020-03-16 16:03:42 +0200

    Fixes

--- install-vscode.sh
+++ install-vscode.sh
@@ -13,29 +13,9 @@ sudo npm install -g typescript
 wget --content-disposition https://go.microsoft.com/fwlink/?LinkID=760868
 sudo apt -y install ./code_*.deb
 
-VS_CODE_EXT="maelvalais.autoconf
-ms-azuretools.vscode-docker
-ms-python.python
-ms-vscode.cpptools
-NorbertKlein.taskmarks
-Nuuf.theme-hackershaze
-Poorchop.theme-darktooth
-asabil.meson
-maziac.asm-code-lens
-NathanRidley.autotrim
-redhat.java
-slevesque.vscode-hexdump
-thiagoabreu.vala
-VisualStudioExptTeam.vscodeintellicode
-vscjava.vscode-java-debug
-vscjava.vscode-java-dependency
-vscjava.vscode-java-pack
-vscjava.vscode-java-test
-vscjava.vscode-maven
-wildboar.asn1"
-
-for vsext in $VS_CODE_EXT;
+VSCODE_EXT_FL="$SCRIPT_DIR/conf/vscode-ext.txt"
+while IFS= read -r line
 do
-	code --install-extension $vsext
-done
+	code --install-extension "$line"
+done < "$VSCODE_EXT_FL"
 
