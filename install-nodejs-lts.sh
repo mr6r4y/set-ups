b7d0c4192de013c4655ad23cc1babe3e64d7aba1
mr6r4y <mr6r4y@gmail.com>
2022-08-19 12:09:43 +0300

    Add NodeJS LTS install script

---
+++ install-nodejs-lts.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+set -e
+OLDDIR=$(pwd)
+
+curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+sudo apt-get install -y nodejs
+nodejs -v
